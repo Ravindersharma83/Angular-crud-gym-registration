f6c47a8bb0374e7398978f998ac37c344867978d
ravindersharma83 <ravinder200198@gmail.com>
2023-06-02 09:56:23 +0530

    creating registration and registration view component to add and view gym registration data

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,9 @@ import { RegistrationListComponent } from './registration-list/registration-list
 import { UserDetailComponent } from './user-detail/user-detail.component';
 import { NgToastModule } from 'ng-angular-popup';
 import { NgConfirmModule } from 'ng-confirm-box';
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
@@ -19,6 +22,11 @@ import {MatRadioModule} from '@angular/material/radio';
 import {MatSelectModule} from '@angular/material/select';
 import {MatDatepickerModule} from '@angular/material/datepicker';
 import { MatNativeDateModule } from '@angular/material/core';
+import {MatTableModule} from '@angular/material/table';
+import {MatPaginatorModule} from '@angular/material/paginator';
+import {MatSortModule} from '@angular/material/sort';
+
+
 
 
 
@@ -36,6 +44,9 @@ import { MatNativeDateModule } from '@angular/material/core';
     BrowserAnimationsModule,
     NgToastModule,
     NgConfirmModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
 
     MatToolbarModule,
     MatIconModule,
@@ -46,6 +57,9 @@ import { MatNativeDateModule } from '@angular/material/core';
     MatSelectModule,
     MatDatepickerModule,
     MatNativeDateModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule,
 
 
   ],
--- src/app/create-registration/create-registration.component.ts
+++ src/app/create-registration/create-registration.component.ts
@@ -1,11 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
+import { ApiService } from '../services/api.service';
+import { NgToastService } from 'ng-angular-popup';
+import { ActivatedRoute } from '@angular/router';
+import { User } from '../model/user.model';
 
 @Component({
   selector: 'app-create-registration',
   templateUrl: './create-registration.component.html',
   styleUrls: ['./create-registration.component.scss']
 })
-export class CreateRegistrationComponent {
+export class CreateRegistrationComponent implements OnInit {
   public packages:string[] = ["Monthly","Quarterly","Yearly"];
   public genders:string[] = ["Male","Female"];
 
@@ -17,4 +22,103 @@ export class CreateRegistrationComponent {
     "Sugar Craving Body",
     "Fitness"
   ]
+
+  // creating Reactive form
+  public registerForm !: FormGroup;
+  public userIdToUpdate !: number;
+  public isUpdateActive: boolean = false;
+  // injecting FormBuilder in the constructor
+  constructor(private fb:FormBuilder, private api:ApiService, private activatedRoute:ActivatedRoute, private toastService : NgToastService){
+
+  }
+
+  ngOnInit(): void {
+    this.registerForm = this.fb.group({
+      firstName: [''],
+      lastName: [''],
+      email: [''],
+      mobile: [''],
+      weight: [''],
+      height: [''],
+      bmi: [''],
+      bmiResult: [''],
+      gender: [''],
+      requireTrainer: [''],
+      package: [''],
+      important: [''],
+      haveGymBefore: [''],
+      enquiryDate: ['']
+    });
+
+    this.registerForm.controls['height'].valueChanges.subscribe(res=>{
+      // console.log(res);
+      this.calculateBmi(res);
+      
+    })
+
+    this.activatedRoute.params.subscribe(val=>{
+      this.userIdToUpdate = val['id'];
+      this.api.getRegisteredUserId(this.userIdToUpdate)
+      .subscribe(res=>{
+        this.isUpdateActive = true;
+        this.fillFormToUpdate(res);
+      })
+    })
+  }
+
+  submit(){
+    // console.log(this.registerForm.value);
+    this.api.postRegistration(this.registerForm.value)
+    .subscribe(res=>{
+      this.toastService.success({detail:"Success", summary:"Enquiry Added!", duration:3000})
+    })
+
+    
+  }
+
+  update(){
+    
+  }
+
+  calculateBmi(value: number) {
+    const weight = this.registerForm.value.weight; // weight in kilograms
+    const height = value; // height in meters
+    const bmi = weight / (height * height);
+    this.registerForm.controls['bmi'].patchValue(bmi);
+    switch (true) {
+      case bmi < 18.5:
+        this.registerForm.controls['bmiResult'].patchValue("Underweight");
+        break;
+      case (bmi >= 18.5 && bmi < 25):
+        this.registerForm.controls['bmiResult'].patchValue("Normal");
+        break;
+      case (bmi >= 25 && bmi < 30):
+        this.registerForm.controls['bmiResult'].patchValue("Overweight");
+        break;
+
+      default:
+        this.registerForm.controls['bmiResult'].patchValue("Obese");
+        break;
+    }
+  }
+
+  fillFormToUpdate(user: User) {
+    this.registerForm.setValue({
+      firstName: user.firstName,
+      lastName: user.lastName,
+      email: user.email,
+      mobile: user.mobile,
+      weight: user.weight,
+      height: user.height,
+      bmi: user.bmi,
+      bmiResult: user.bmiResult,
+      gender: user.gender,
+      requireTrainer: user.requireTrainer,
+      package: user.package,
+      important: user.important,
+      haveGymBefore: user.haveGymBefore,
+      enquiryDate: user.enquiryDate
+    })
+  }
+
 } 
